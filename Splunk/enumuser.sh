71f917dfe0af085506903c470e4a67d1314be172
Aria <157536992+Aria-C900@users.noreply.github.com>
2025-02-19 18:20:48 -0500

    Update enumuser.sh
    + minor edits

--- Splunk/enumuser.sh
+++ Splunk/enumuser.sh
@@ -7,37 +7,31 @@ chmod +x "$0"
 
 # Check if script is run as root
 
-if [ "$EUID" -ne 0]; then
+if [ "$EUID" -ne 0 ]; then
     echo "Please run this script as root!"
     exit 1
-
 fi
 
-OUTPUT_FILE="enumeration_results_$(endata +%Y%m%d_%H%M%S).txt" #results are dumped into enumeration_results
+OUTPUT_FILE="enumeration_results_$(date +%Y%m%d_%H%M%S).txt" #results are dumped into enumeration_results
 
+#Enumerates users on the splunk CLI and puts them into enumeration_results_.txt + includes per-user cron jobs as well
 echo "=== Enumerating Users ===" | tee -a "$OUTPUT_FILE"
 awk -F':' '{print $1}' /etc/passwd | tee -a "$OUTPUT_FILE"
 
-#cat /etc/passwd | cut d: -f1
-#TROUBLESHOOT: under this, cut -d: is not recognized
-
-echo -e "\n=== Enumerating Cron Jobs ===" | tee -a "$OUTPUT_FILE"
-#TROUBLESHOOT: cut: you must specifiy a list of bytes/characters or field 
-#for user in $(cut -d: f1 /etc/passwd); do
+# Per-user cron jobs
+echo -e "\n[+] Per-user Cron Jobs:" | tee -a "$OUTPUT_FILE"
 for user in $(awk -F':' '{print $1}' /etc/passwd); do
-    echo "Cron jobs for user: $user" | tee-a "$OUTPUT_FILE"
-    crontab -l -u "$user" 2>/dev/null | tee -a "$OUTPUT_FI.E" || echo "No cron jobs for $user" | tee -a "$OUTPUT_FILE"
+    echo -e "\n[*] Cron jobs for user: $user" | tee -a "$OUTPUT_FILE"
+    crontab -l -u "$user" 2>/dev/null | tee -a "$OUTPUT_FILE" || echo "No cron jobs for $user" | tee -a "$OUTPUT_FILE"
 done
-    #cronfile="/var/spool/cron/crontabs/$user"
-    #if [ -f "$cronfile" ]; then
-        #echo -e "\n[+] Cron jobs for user: $user"
-        #cat "$cronfile"
-    #fi
-#done
 
-#system wide cron jobs works as expected
-echo -e "\n=== System-wide Cron Jobs ==="
-cat /etc/crontab
-cat /etc/cron.d/*
+
+
+# System-wide cron jobs
+echo -e "\n[+] System-Wide Cron Jobs (/etc/crontab and /etc/cron.d/*):" | tee -a "$OUTPUT_FILE"
+cat /etc/crontab 2>/dev/null | tee -a "$OUTPUT_FILE"
+ls -l /etc/cron.d 2>/dev/null | tee -a "$OUTPUT_FILE"
+
+
 
 echo "Results saved to $OUTPUT_FILE"
