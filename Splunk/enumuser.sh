2a60a939eb86c6d746d8ac151cbfc34eaff74f24
Aria <157536992+Aria-C900@users.noreply.github.com>
2025-02-19 01:21:07 -0500

    Update enumuser.sh

--- Splunk/enumuser.sh
+++ Splunk/enumuser.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# makes script executable
+#NOT COMPLETED!!!!!!!!!!!!
 
-chmod +x enumuser.sh
+# make sure to make the script executable before running
+# sudo chmod +x enumuser.sh
 
 # Check if script is run as root
 
@@ -14,8 +15,10 @@ fi
 
 echo "=== Enumerating Users ==="
 cat /etc/passwd | cut d: -f1
+#TROUBLESHOOT: under this, cut -d: is not recognized
 
 echo -e "\n=== Enumerating Cron Jobs ==="
+#TROUBLESHOOT: cut: you must specifiy a list of bytes/characters or field 
 for user in $(cut -d: f1 /etc/passwd); do
     cronfile="/var/spool/cron/crontabs/$user"
     if [ -f "$cronfile" ]; then
@@ -24,6 +27,7 @@ for user in $(cut -d: f1 /etc/passwd); do
     fi
 done
 
+#system wide cron jobs works as expected
 echo -e "\n=== System-wide Cron Jobs ==="
 cat /etc/crontab
 cat /etc/cron.d/*
