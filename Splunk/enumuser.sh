49d6ab4baae78f4e7210660239561634de722300
Aria <157536992+Aria-C900@users.noreply.github.com>
2025-02-19 17:48:52 -0500

    Update enumuser.sh

--- Splunk/enumuser.sh
+++ Splunk/enumuser.sh
@@ -13,18 +13,20 @@ if [ "$EUID" -ne 0]; then
 
 fi
 
-echo "=== Enumerating Users ==="
-awk -F':' '{print $1}' /etc/passwd
+OUTPUT_FILE="enumeration_results_$(endata +%Y%m%d_%H%M%S).txt" #results are dumped into enumeration_results
+
+echo "=== Enumerating Users ===" | tee -a "$OUTPUT_FILE"
+awk -F':' '{print $1}' /etc/passwd | tee -a "$OUTPUT_FILE"
 
 #cat /etc/passwd | cut d: -f1
 #TROUBLESHOOT: under this, cut -d: is not recognized
 
-echo -e "\n=== Enumerating Cron Jobs ==="
+echo -e "\n=== Enumerating Cron Jobs ===" | tee -a "$OUTPUT_FILE"
 #TROUBLESHOOT: cut: you must specifiy a list of bytes/characters or field 
 #for user in $(cut -d: f1 /etc/passwd); do
 for user in $(awk -F':' '{print $1}' /etc/passwd); do
-    echo "Cron jobs for user: $user"
-    crontab -l -u "$user" 2>/dev/null || echo "No cron jobs for $user"
+    echo "Cron jobs for user: $user" | tee-a "$OUTPUT_FILE"
+    crontab -l -u "$user" 2>/dev/null | tee -a "$OUTPUT_FI.E" || echo "No cron jobs for $user" | tee -a "$OUTPUT_FILE"
 done
     #cronfile="/var/spool/cron/crontabs/$user"
     #if [ -f "$cronfile" ]; then
