fe4844d2f092089d7d95a3050780341aea49b9be
Aria <157536992+Aria-C900@users.noreply.github.com>
2025-02-22 09:41:49 -0500

    Create PAN-OS.sh
    + basic config of network on pan os CLI

---
+++ Networking/PAN-OS.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script applies the configuration changes using the CLI tool.
+# Replace "cli" with the actual command if different.
+# Rough draft w/ AI look over, special thanks to Ronnie!
+
+cli <<'EOF'
+configure
+delete address "Google DNS"
+
+rename address "Internal" to "Internal NetID"
+rename address "Public" to "Public NetID"
+rename address "User" to "User NetID"
+
+set address "Cloudflare DNS" ip-netmask 1.1.1.1/32
+set address "2019 Docker" ip-netmask 172.20.240.10/32
+set address "Debian" ip-netmask 172.20.240.20/32
+set address "Internal Gateway" ip-netmask 172.20.240.254/32
+set address "Ubuntu Srvr" ip-netmask 172.20.242.10/32
+set address "2019 AD" ip-netmask 172.20.242.100/32
+set address "User Gateway" ip-netmask 172.20.242.254/32
+set address "Splunk" ip-netmask 172.20.241.20/32
+set address "CentOS" ip-netmask 172.20.241.30/32
+set address "Fedora" ip-netmask 172.20.241.40/32
+set address "Windows 10" ip-netmask 172.31.37.5
+
+commit
+EOF
